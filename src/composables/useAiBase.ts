98f506bf6c660c486762cdebc8a3f60469b11ec7
二五六仔 <534496845@qq.com>
2023-06-30 09:20:27 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/composables/useAiBase.ts
    
    Changes not staged for commit:
    	deleted:    nodeServer/index.js
    	modified:   nodeServer/package.json
    	modified:   nodeServer/pnpm-lock.yaml
    	modified:   src/App.vue
    	modified:   src/api/modules/localMethods.ts
    	modified:   src/api/modules/slack.ts
    	modified:   src/composables/useCommand.ts
    	modified:   src/composables/useOpenAi.ts
    	modified:   src/composables/useSlack.ts
    	modified:   src/init.ts
    	modified:   src/types/auto-apis.d.ts
    	modified:   src/utils/index.ts
    	modified:   vite.config.ts
    
    Untracked files:
    	nodeServer/nodeserver-linux
    	nodeServer/nodeserver-macos
    	nodeServer/src/

--- src/composables/useAiBase.ts
+++ src/composables/useAiBase.ts
@@ -1,58 +1,65 @@
-import { nowSessionName, saveSession, useSession } from "@/pages/Home/com/chatLeft"
-import ClipboardJS from "clipboard"
-import { ElMessage } from "element-plus"
-export const modelsInfo = ref<GPT3Models>()
+import {
+  nowSessionName,
+  saveSession,
+  useSession,
+} from "@/pages/Home/com/chatLeft";
+import ClipboardJS from "clipboard";
+import { ElMessage } from "element-plus";
+export const modelsInfo = ref<GPT3Models>();
 abstract class useAiBase {
-  editorArea = ref<string>('')
-  textList = ref<Partial<chatCompletion>[]>([])
+  editorArea = ref<string>("");
+  textList = ref<Partial<chatCompletion>[]>([]);
   stop = async (item: chatCompletion) => {
-    saveSession()
-    if (item.sessionIng === '已终止') {
-      return
+    saveSession();
+    if (item.sessionIng === "已终止") {
+      return;
     }
-    item.sessionIng = '已终止'
-    item.signal.abort()
-  }
+    item.sessionIng = "已终止";
+    item.signal.abort();
+  };
   setTextList = (textList: Ref<Partial<chatCompletion>[]>) => {
-    this.textList = textList
-  }
+    this.textList = textList;
+  };
   addUserSession = async (prompt: string) => {
-    const { addSession, sessionBuilder } = useSession(this.textList)
+    const { addSession, sessionBuilder } = useSession(this.textList);
     if (this.textList.value.length === 0) {
-      const name = Date.now().toString()
-      addSession(name)
-      sessionBuilder()
-      nowSessionName.value = name
+      const name = Date.now().toString();
+      addSession(name);
+      sessionBuilder();
+      nowSessionName.value = name;
     }
-    this.editorArea.value = ''
-    const time = Date.now()
+    this.editorArea.value = "";
+    const time = Date.now();
     this.textList.value!.push({
       message: prompt,
       title: prompt,
-      role: 'user',
-      owned_by: 'my',
-      createdTime: Dayjs(time).format('YYYY-MM-DD HH:mm:ss'),
+      role: "user",
+      owned_by: "my",
+      createdTime: Dayjs(time).format("YYYY-MM-DD HH:mm:ss"),
       edit: false,
-      sessionIng: '已完成',
-      created: time
-    })
-    saveSession()
-    await nextTick()
-    const clipboard = new ClipboardJS('.copy-' + time);
-    clipboard.on('success', () => {
+      sessionIng: "已完成",
+      created: time,
+    });
+    saveSession();
+    await nextTick();
+    const clipboard = new ClipboardJS(".copy-" + time);
+    clipboard.on("success", () => {
       ElMessage({
         type: "success",
-        message: "复制成功！"
-      })
-    })
-  }
+        message: "复制成功！",
+      });
+    });
+  };
   ModelInfoInit = async () => {
-    const json: GPT3Models = await (await fetch('modelsInfo.json')).json()
-    modelsInfo.value = json
-  }
-  abstract setModel(Model: Model): void
-  abstract getModel(): any
-  abstract listModels: (setData: any) => any
-  abstract send(prompt?: string, textList?: Ref<Partial<chatCompletion>[]>): void
+    const json: GPT3Models = await (await fetch("modelsInfo.json")).json();
+    modelsInfo.value = json;
+  };
+  abstract setModel(Model: Model): void;
+  abstract getModel(): any;
+  abstract listModels: (setData: any) => any;
+  abstract send(
+    prompt?: string,
+    textList?: Ref<Partial<chatCompletion>[]>
+  ): void;
 }
-export default useAiBase
+export default useAiBase;
